aede3e8d64405bd47d6a6410c0b5ecc8194b4b42
bijayapatra450-design <bijayapatra450@gmail.com>
2025-11-26 12:00:00 +0530

    Update grafna-promethous.sh

--- grafna-promethous.sh
+++ grafna-promethous.sh
@@ -137,7 +137,7 @@ route:
 receivers:
   - name: pagerduty
     pagerduty_configs:
-      - routing_key: "4bf6bcd8cb9b4607d047c4b6c2d333d6" #add key pagerduty 
+      - routing_key: "6450cf19d4bf4506d038ddf1713079c2" #add key pagerduty 
         severity: "critical"
 EOF
 
